63d4ec655566c9707275955d411301e035ec39ac
vbloke <vbloke@gmail.com>
2014-11-24 17:43:08 +0000

    new css

---
+++ single.php
@@ -0,0 +1,70 @@
+<?php get_header(); ?>
+
+<div class="row">
+
+	<div class="nine columns">
+	
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	
+		<h2><?php the_title(); ?></h2>	
+		<div class="person">
+			<?php
+				if ( has_post_thumbnail() ) {
+					the_post_thumbnail('speaker');
+				}	
+			?>
+			<?php the_content(); ?>
+		</div>
+	<?php endwhile; ?>
+	<div class="row">
+		<div class="six columns">
+			<?php previous_post('&laquo; %', '', 'yes'); ?>
+		</div>
+		<div class="six columns">
+			<?php next_post('% &raquo; ', '', 'yes'); ?>
+		</div>
+	</div>	
+	<?php endif; ?>
+	
+	</div>	
+	
+	<div class="three columns side">
+		
+		<div class="row">
+			<div class="twelve columns">
+				<ul>
+					<?php
+					// Find connected pages
+					$connected = new WP_Query( array(
+						'connected_type' => 'pdf_to_job',
+						'connected_items' => get_queried_object(),
+						'nopaging' => true,
+					) );
+					
+					// Display connected pages
+					if ( $connected->have_posts() ) :
+					?>
+					<li class="widget">
+					<h5 class="widget">Related Documents:</h3>
+					<ul class="menu related">
+					<?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
+					    <li><a href="<?php global $post; $text = get_post_meta( $post->ID, '_cmb_pdf', true ); echo $text; ?>"><?php the_title(); ?></a></li>
+					<?php endwhile; ?>
+					</ul>
+					</li>
+					<?php 
+					// Prevent weirdness
+					wp_reset_postdata();
+					
+					endif;
+					?>	
+					<?php dynamic_sidebar( 'homepage' ); ?>
+				</ul>
+			</div>
+		</div>		
+
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
