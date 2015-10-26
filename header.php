cdef9d8d0ee7040eb3f464fbd354f206c2568d8f
vbloke <vbloke@gmail.com>
2015-10-26 08:12:23 +0000

    removed pdfs from secondary mobile menu

--- header.php
+++ header.php
@@ -66,31 +66,6 @@ if( $detect->isMobile() && !$detect->isTablet() ){
 	</div>
 	<div class="row" id="subBarContent">
 		<ul>
-			<?php
-			// Find connected pages
-			$connected = new WP_Query( array(
-				'connected_type' => 'pdf_to_page',
-				'connected_items' => get_queried_object(),
-				'nopaging' => true,
-			) );
-
-			// Display connected pages
-			if ( $connected->have_posts() ) :
-			?>
-			<li class="widget">
-			<h5 class="widget">Related Documents:</h3>
-			<ul class="menu related">
-			<?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
-			    <li><a href="<?php global $post; $text = get_post_meta( $post->ID, '_cmb_pdf', true ); echo $text; ?>" download><?php the_title(); ?></a></li>
-			<?php endwhile; ?>
-			</ul>
-			</li>
-			<?php
-			// Prevent weirdness
-			wp_reset_postdata();
-
-			endif;
-			?>
 			<?php dynamic_sidebar( 'homepage' ); ?>
 		</ul>
 	</div>
