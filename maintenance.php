fa38d883c49d48dc0ac7e888737422e524ca36c8
vbloke <vbloke@gmail.com>
2016-10-26 09:10:38 +0100

    styled maintenance page

---
+++ maintenance.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+<!--base href="https://.org.uk/"-->
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="https://conwayhall.org.uk/wp-content/themes/conwayhall/css/normalize.css" />
+<link rel="stylesheet" type="text/css" href="https://conwayhall.org.uk/wp-content/themes/conwayhall/css/base.css" />
+<link rel="stylesheet" type="text/css" href="https://conwayhall.org.uk/wp-content/themes/conwayhall/style.css" />
+</head>
+
+<body>
+
+<div class="navbar" id="nav1" role="navigation">
+	<div class="row"> </div>
+</div>
+<div class="header" role="banner">
+	<div class="row">
+		<div class="twelve columns">
+			<h1><i class="ch-logo"></i></h1>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="twelve columns center" class="main">
+		<center>
+			<h2>We're doing some routine maintenance.</h2>
+			<p>Conway Hall's site is undergoing a brief bit of maintenance that will last 15 minutes at the very most.</p>
+			<p>We apologise for the inconvenience, we're doing out best to get things back to working order for you.</p>
+		</center>
+	</div>
+</div>
